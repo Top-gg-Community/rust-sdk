a889bc03c61826a1a633c01f642a7ff061c85277
null8626 <vierofernando9@gmail.com>
2023-12-16 12:33:30 +0700

    fix: fix issues with rocket

--- src/webhook/rocket.rs
+++ src/webhook/rocket.rs
@@ -1,26 +1,28 @@
-use crate::IncomingVote;
+use crate::{IncomingVote, Vote};
 use rocket::{
-  data::{Data, FromDataSimple, Outcome},
+  data::{Data, FromData, Outcome},
   http::Status,
   request::Request,
+  serde::json::Json,
 };
 
 #[cfg_attr(docsrs, doc(cfg(feature = "rocket")))]
-impl FromDataSimple for IncomingVote {
+#[rocket::async_trait]
+impl<'r> FromData<'r> for IncomingVote {
   type Error = ();
 
-  fn from_data(request: &Request<'_>, data: Data) -> Outcome<Self, Self::Error> {
+  async fn from_data(request: &'r Request<'_>, data: Data<'r>) -> Outcome<'r, Self> {
     let headers = request.headers();
 
     if let Some(authorization) = headers.get_one("Authorization") {
-      if let Ok(vote) = serde_json::from_reader(data.open()) {
+      if let Outcome::Success(vote) = <Json<Vote> as FromData>::from_data(request, data).await {
         return Outcome::Success(Self {
           authorization: authorization.to_owned(),
-          vote,
+          vote: vote.into_inner(),
         });
       }
     }
 
-    Outcome::Failure((Status::Unauthorized, ()))
+    Outcome::Error((Status::Unauthorized, ()))
   }
 }
