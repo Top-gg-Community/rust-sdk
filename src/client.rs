ec5b9c1934a086cd479463436f7abdcecb64ae12
Valentine Briese <valentinegb@icloud.com>
2024-07-31 11:41:43 -0700

    Implement `Debug` for `Client` and `InnerClient`

--- src/client.rs
+++ src/client.rs
@@ -33,6 +33,7 @@ macro_rules! api {
   };
 }
 
+#[derive(Debug)]
 pub struct InnerClient {
   http: reqwest::Client,
   token: String,
@@ -125,6 +126,7 @@ impl InnerClient {
 
 /// A struct representing a [Top.gg API](https://docs.top.gg) client instance.
 #[must_use]
+#[derive(Debug)]
 pub struct Client {
   inner: SyncedClient,
 }
